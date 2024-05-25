09cbdc88054b4dc8e143c86b92f7638a07ac8c09
Daniel Hassler <daniel@dhslr.de>
2024-05-25 08:20:56 +0200

    Add elixir plugins/configs

--- init.lua
+++ init.lua
@@ -592,6 +592,8 @@ require('lazy').setup({
             },
           },
         },
+        elixirls = {},
+        tailwindcss = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -835,7 +837,21 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'vim',
+        'vimdoc',
+        'elixir',
+        'eex',
+        'heex',
+        'javascript',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -885,7 +901,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
